939bdbeee989625e6d4a831de4f03332c95b1b07
Fernanda Esquivel <esq21542@uvg.edu.gt>
2021-09-28 08:55:13 -0600

    Clase madre e hijas
    Se creo la clase madre (combatiente) y sus clases hijas (enemigo y jugador)

---
+++ Enemigo.java
@@ -0,0 +1,53 @@
+
+/* Nombre: Enemigo.java
+ * Programador: Fernanda Esquivel (esq21542@uvg.edu.gt).
+ * Lenguaje: Java
+ * Recursos: Visual Studio Code
+ * Historial: Finalizado el 04.09.2021 */
+
+public class Enemigo extends Combatiente
+{
+    private String habilidad;
+    private int damageH;
+    private boolean objetivo;
+    
+    public Enemigo()
+    {
+        objetivo = false;        
+    }
+
+    public String getHabilidad()
+    {
+        return habilidad;
+    }
+
+    public boolean getObjetivo()
+    {
+        return objetivo;
+    }
+
+    public void setObjetivoT()
+    {
+        objetivo = true;
+    }
+
+    public void setObjetivoF()
+    {
+        objetivo = false;
+    }
+
+    public int getDamageH()
+    {
+        return damageH;
+    }
+
+    public void atacar(Jugador j)
+    {
+        j.setVida(this.getAtaque());
+    }
+
+    public void usarHabilidad(Jugador j)
+    {
+        j.setVida(damageH);
+    }
+}
---
+++ Jugador.java
@@ -0,0 +1,17 @@
+
+/* Nombre: Jugador.java
+ * Programador: Fernanda Esquivel (esq21542@uvg.edu.gt).
+ * Lenguaje: Java
+ * Recursos: Visual Studio Code
+ * Historial: Finalizado el 04.09.2021 */
+
+public class Jugador extends Combatiente
+{
+    private Item item;
+    
+    public Jugador()
+    {
+
+    }
+
+}
